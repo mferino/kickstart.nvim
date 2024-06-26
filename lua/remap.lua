faaf96c570f4c08c5680b9fd7bacba45f24ceb9c
mferino <mdference@gmail.com>
2024-06-26 10:53:17 -0500

    couple new remaps ftw

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,8 +1,16 @@
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+local set = vim.keymap.set
+
+set("n", "<C-k>", "<cmd>cnext<CR>zz")
+set("n", "<C-j>", "<cmd>cprev<CR>zz")
 
 -- man pages
-vim.keymap.set("n", "<leader>mp", function()
+set("n", "<leader>mp", function()
   local word = vim.fn.expand("<cword>")
   vim.cmd("Man " .. word)
 end, { noremap = true, silent = true, desc = "[M]an[P]ages" })
+
+-- window controls
+set("n", "<M-,>", "<c-w>5<")
+set("n", "<M-.>", "<c-w>5>")
+set("n", "<M-t>", "<C-W>+")
+set("n", "<M-s>", "<C-W>-")
